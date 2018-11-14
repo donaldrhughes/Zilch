e6d609a150951733bb58f91df983bd317ec4c4be
myrean <dhughes1@uci.edu>
2018-11-13 22:48:36 -0800

    POINTS

--- assets/javascript/zilch.js
+++ assets/javascript/zilch.js
@@ -3,16 +3,11 @@ $(document).ready(function () {
 
 
 
-            // Declare Global Vars
-            //================================================
-      
-    var turn = 1;
-    var round = 1;
-    var rollcount = 0;
-    var totalgames = 0;
-    var diceResult = [];
+    // Declare Global Vars
+    //================================================
 
- 
+
+    var totalgames = 0;
 
     // Player Object
     var player = {
@@ -28,8 +23,30 @@ $(document).ready(function () {
 
 
 
-        //Functions
-        //======================================================================
+    //Functions
+    //======================================================================
+
+    // Start the Game
+
+    function startGame() {
+        var turn = 1;
+        player.bank = 0;
+        $("#turn-text").text(turn);
+        var round = 1;
+        $("#round-text").text(round);
+        var rollcount = 0;
+        var diceResult = [];
+        var holdArea = [];
+        var dieCount = [];
+        // Roll Button click handler
+        $("#roll-button").on("click", function () {
+            rollcount++;
+            $("#rollcount-text").text(rollcount);
+
+            rolldice();
+        })
+    }
+
 
     //Roll the dice!
     function rolldice() {
@@ -37,54 +54,148 @@ $(document).ready(function () {
         var diceResult = Array.from({ length: 6 }, () => Math.floor(Math.random() * 6) + 1);
         var results = [];
         results = diceResult.join(" ");
-        
-        // updateHTML();
+        var dieCount = [];
+        scoring(diceResult, dieCount);
         $("#result-text").text(results);
-        console.log(diceResult);
-        // for (var i = 0; i < diceResult.length; i++) {
 
 
     };
 
-       //Jquery for HTML updates
-    function assignVars(){
-        var resultText = $("#result-text");
-        var turnText = $("#turn-text");
-        var roundText = $("#round-text");
-        var rollcountText = $("#rollcount-text");
-        var bankText = $("#bank-text");
-    
+    //Jquery for HTML updates
+    // function assignVars() {
+    //     var resultText = $("#result-text");
+    //     var turnText = $("#turn-text");
+    //     var roundText = $("#round-text");
+    //     var rollcountText = $("#rollcount-text");
+    //     var bankText = $("#bank-text");
+
+    // }
+
+
+
+    // Hold Dice
+
+
+
+    //Scoring
+    function scoring(diceResult) {
+        countDice(diceResult);
+        updateBank();
+
+
+
+        console.log("Player Bank" + " " + player.bank);
     }
 
-        // updateHTML();
-    function updateHTML(){
-        $("#turn-text").text(turn);
-        $("#round-text").text(round);
-        $("#rollcount-text").text(rollcount);
+
+    function countDice(diceResult) {
+        var count1 = 0;
+        var count2 = 0;
+        var count3 = 0;
+        var count4 = 0;
+        var count5 = 0;
+        var count6 = 0;
+
+        console.log(diceResult);
+        for (j = 0; j < diceResult.length; j++) {
+            if (diceResult[j] == 1)
+                count1++;
+
+
+
+            if (diceResult[j] == 2)
+                count2++;
+
+
+            if (diceResult[j] == 3)
+                count3++;
+
+
+            if (diceResult[j] == 4)
+                count4++;
+
+
+            if (diceResult[j] == 5)
+                count5++;
+
+
+
+            if (diceResult[j] == 6)
+                count6++;
+        };
+        dieCount = [count1, count2, count3, count4, count5, count6];
+
+
+
     }
 
 
- //Resets
-    function reset(){
-      var turn = 1;
-      var round = 1;
-      var rollcount = 0;
-      var totalgames = 0;
+    function updateBank() {
+
+        if (dieCount[0] == 1) {
+            player.bank += 100;
+        }
+
+        if (dieCount[0] == 2) {
+            player.bank += 200;
+        }
+        if (dieCount[0] == 3) {
+            player.bank += 1000;
+        }
+        if (dieCount[0] == 4) {
+            player.bank += 2500;
+
+        }
+        if (dieCount[0] == 5) {
+            player.bank += 5000;
+
+        }
+        if (dieCount[0] == 6) {
+            player.bank += 10000;
+
+        }
+        if (dieCount[4] == 1) {
+            player.bank += 50;
+        }
+
+        if (dieCount[4] == 2) {
+            player.bank += 100;
+        }
+        if (dieCount[4] == 3) {
+            player.bank += 500;
+        }
+        if (dieCount[4] == 4) {
+            player.bank += 2000;
+
+        }
+        if (dieCount[4] == 5) {
+            player.bank += 5000;
+
+        }
+        if (dieCount[4] == 6) {
+            player.bank += 10000;
+
+        }
+
+        console.log(dieCount);
+
     }
 
-//Main Section
-//===================================================
-reset();
-assignVars();
-updateHTML();
+    //Check if won
 
 
-// Roll Button click handler
+    //Check if over 10,000
 
 
-$("#roll-button").on("click", function () {
-    rollcount++;
-    updateHTML();
-    rolldice();
+
+
+    //Main Section
+    //===================================================
+    startGame();
+
+
+
+
+
+
 })
-});
